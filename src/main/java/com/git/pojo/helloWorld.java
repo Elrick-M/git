8ba2eb2844fa998e25de14425c769ca8da82f522
Elrick-M <Elrick-M@qq.com>
2022-08-20 21:06:05 +0800

    third-commit
    Signed-off-by: Elrick-M <Elrick-M@qq.com>

---
+++ src/main/java/com/git/pojo/helloWorld.java
@@ -0,0 +1,10 @@
+package com.git.pojo;
+
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world01");
+        System.out.println("hello world02");
+        System.out.println("hello world03");
+    }
+}
