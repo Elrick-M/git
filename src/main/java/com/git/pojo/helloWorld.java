043b265228fd644aabfd34c1ff9007689b338542
Elrick-M <111121246+Elrick-M@users.noreply.github.com>
2022-08-29 21:24:41 +0800

    fix-01

--- src/main/java/com/git/pojo/helloWorld.java
+++ src/main/java/com/git/pojo/helloWorld.java
@@ -11,5 +11,6 @@ public class helloWorld {
         System.out.println("hello world06");
         System.out.println("hello master");
         System.out.println("hello hotfix");
+        System.out.println("好好学习");
     }
 }
